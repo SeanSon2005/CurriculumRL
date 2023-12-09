d50941e324422eefb00a577a1a8601e027808d2b
nesl <swapnilsayansaha@ieee.org>
2023-12-08 16:25:17 -0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+I have not setup read me, but you need to get gymnasium! I recommend a python virtual environment :D
---
+++ environment.py
@@ -0,0 +1,54 @@
+import gymnasium as gym
+from gymnasium import spaces
+from action import Tasks
+import numpy as np 
+
+MAP_SIZE = 30
+MAX_ITEM_WEIGHT = 2
+
+MAX_ITEMS = 5
+MAX_ROBOTS = 1
+
+class CustomEnv(gym.Env):
+	def __init__(self):
+		self.action_space = spaces.Dict(
+            {
+				"subtask": spaces.Discrete(len(Tasks)),
+            }
+        )
+		self.observation_space = spaces.Dict(
+            {
+                "ego_location": spaces.Box(low=-np.infty, high=np.infty, shape=(2,), dtype=np.int16),
+                "objects_held": spaces.Discrete(3, start=-1),
+				
+                "num_items": spaces.Discrete(MAX_ITEMS),
+                "item_info": spaces.Dict(
+                    {
+                        "weight": spaces.Discrete(MAX_ITEM_WEIGHT),
+                        "isDangerous": spaces.Discrete(1),
+                        "danger_confidence": spaces.Box(low=0, high=1, shape=(1,), dtype=float),
+                        "location": spaces.Box(low=-np.infty, high=np.infty, shape=(2,), dtype=np.int16),
+                    }
+                ),
+
+                "num_neighbors": spaces.Discrete(MAX_ROBOTS),
+                "neighbors_info": spaces.Dict(
+                    {
+                        "location": spaces.Box(low=-np.infty, high=np.infty, shape=(2,), dtype=np.int16)
+                    }
+                ),
+				
+                "strength": spaces.Discrete(len(self.map_config['all_robots']) + 2),
+                "num_messages": spaces.Discrete(100)
+            }
+        )
+        
+	def step(self, action):
+		pass
+	def render(self):
+		pass
+	def reset(self):
+		pass
+
+
+
